c1bf669ce250d307ba0e65146153b2517c04799d
cheng <cheng.xue@anu.edu.au>
2021-08-30 22:27:09 +1000

    remove old agents

--- Agents/TestPythonHeuristicAgent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-agent="$1"
-templates=$(python Utils/GenerateTemplateName.py)
-for val in $templates; do
-    echo running $val
-    python MultiAgentTestOnly.py --template $val --agent $agent
-done
